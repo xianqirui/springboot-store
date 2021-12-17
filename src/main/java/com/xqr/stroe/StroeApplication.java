bc1fe6e6a2c2727575818707264d43be4a6fb5b0
xqr <xqr@xqr.com>
2021-12-17 10:35:37 +0800

    项目搭建

---
+++ src/main/java/com/xqr/stroe/StroeApplication.java
@@ -0,0 +1,13 @@
+package com.xqr.stroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StroeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StroeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xqr/stroe/StroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqr.stroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StroeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
